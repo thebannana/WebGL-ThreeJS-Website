83fcce70f2e07790fdbfdbd6e5ee55e9608c5c71
Denis Kundo <kundodenis@gmail.com>
2024-12-13 21:50:13 +0100

    Dodan website sa Orbital Controls u 3D sceni

--- Vite/src/main.js
+++ Vite/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import {OrbitControls} from "three/addons/controls/OrbitControls.js";
 
 const canvas = document.getElementById( 'canvas');
 
@@ -13,7 +14,7 @@ camera.position.z = 5;
 //Mesh
 
 const geometry = new THREE.DodecahedronGeometry();
-const material = new THREE.MeshLambertMaterial({color: '#468585'});
+const material = new THREE.MeshLambertMaterial({color: '#468585', emissive:'#468585',});
 const Dodacahedron = new THREE.Mesh(geometry,material);
 
 const boxGeometry = new THREE.BoxGeometry(2,0.1,2);
@@ -34,7 +35,36 @@ scene.add(light);
 
 const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+//add orbit controls
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableDamping = true;
+controls.enablePan = true;
 
 //animate
-renderer.render(scene, camera);
 
+function animate(){
+    requestAnimationFrame(animate);
+
+    Dodacahedron.rotation.x += 0.01;
+    Dodacahedron.rotation.y += 0.01;
+
+    box.rotation.y += 0.005;
+
+    controls.update();
+
+    renderer.render(scene, camera);
+}
+
+//Handle window resizing
+
+window.addEventListener('resize', () =>{
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+} );
+animate();
